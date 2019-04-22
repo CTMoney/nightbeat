f04606f270eccf3dda6b87d0395cf67f403af181
Dillon <dtidwell123@gmail.com>
2019-04-22 17:50:00 -0400

    Dillon Branch Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import Container from './components/Container/Container'
 import Navbar from './components/navbar/Navbar'
+import Splash from './components/pages/splash'
 
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Navbar />
-        <Container />
-      </>
+      <Splash />
     )
   }
 }
