194ecb80b03d2d55e6205680cfcdd539697412a7
Cat <money1062@live.com>
2019-04-24 16:31:33 -0400

    added import.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Container from './components/Container/Container'
 import Navbar from './components/navbar/Navbar'
+import CreateAccount from './components/pages/Create/Account'
 import Splash from './components/pages/splash'
 
 
