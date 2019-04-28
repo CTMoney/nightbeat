aba710de30902b3142f3ef5310f688cc4ce0039a
Cat <money1062@live.com>
2019-04-28 16:49:14 -0400

    new file structure

--- routes/create-account-routes.js
+++ routes/create-account-routes.js
@@ -2,7 +2,7 @@ const db = require('../models')
 
 module.exports = app => {
 
-  app.post("/create/submit", (req, res) => {
+  app.post("/register/submit", (req, res) => {
 
     const { username, email, password } = req.body;
     db.User.create({ "user_name": username, 'user_email': email, 'user_pass': password })
