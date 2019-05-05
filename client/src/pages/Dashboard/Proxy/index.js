6c31c1e33f1014547fe297191542c0eb61748e62
CTMoney <44733797+CTMoney@users.noreply.github.com>
2019-05-05 14:40:08 -0400

    Update index.js

--- client/src/pages/Dashboard/Proxy/index.js
+++ client/src/pages/Dashboard/Proxy/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import API from '../../../util/API'
-<<<<<<< HEAD
 import ProxyTable from '../../../components/ProxyTable'
-=======
->>>>>>> c471dd36d92313315b2159e2841a41f33301aeab
+
 
 class Proxies extends React.Component {
 
@@ -21,11 +19,8 @@ class Proxies extends React.Component {
         })
       }
 
-<<<<<<< HEAD
-      handleProfile = (event) => {
-=======
+
       handleProxy = (event) => {
->>>>>>> c471dd36d92313315b2159e2841a41f33301aeab
         event.preventDefault()
         const { firstname, email, address, city, state, zip, cardname, cardnumber, expmonth, expyear, cvv } = this.state
         API.task(firstname, email, address, city, state, zip, cardname, cardnumber, expmonth, expyear, cvv)
@@ -35,50 +30,15 @@ class Proxies extends React.Component {
         return (
         <>
             <p className="display-4 mt-0">Proxy</p>
-<<<<<<< HEAD
-            <div className="proxy-container">
-                <form className="form-inline">
-=======
+
             <div>
                 <form className="form-inline" onSubmit={this.handleProxy}>
->>>>>>> c471dd36d92313315b2159e2841a41f33301aeab
+
                     <input className="form-control mb-2 mr-sm-2" placeholder="Input Proxy" style={{ minWidth: "200px" }} type="text" name="proxy"/>
                     <button type="submit" className="btn btn-primary mb-2 mr-sm-2">submit</button>
                 </form> 
-                
-<<<<<<< HEAD
             
-<ProxyTable/>
-=======
-                <table className="table table-borderless table-hover table-dark">
-                    <thead>
-                        <tr>
-                            <th scope="col"><input className="form-check-input" type="checkbox" value="" id="checkAll" />checkbox</th>
-                            <th scope="col">ID</th>
-                            <th scope="col">Proxy</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Password</th>
-                            <th scope="col">Response Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">  <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" /> </th>
-                            <th>1</th>
-                            <td>usimportantd1.fogldn.com:11743</td>
-                            <td>Name</td>
-                            <td>gunga :D</td>
-                            <td>700ms</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <form className="form-inline" onSubmit={this.handleProxy}>
-                    <input className="form-control mb-2 mr-sm-2" type="text" style={{ minWidth: "400px" }} placeholder="Delete those with response time slow than.."></input>
-                    <button className="btn btn-primary mb-2 mr-sm-2">submit</button>
-                    <button className="btn btn-danger mb-2 mr-sm-2">Test Proxies</button>
-                </form>
-
->>>>>>> c471dd36d92313315b2159e2841a41f33301aeab
+                <ProxyTable/>
             </div>
         </>
     )}
